ed2064ae573cd25c269daa13d5c7439c61936662
Garvit Singhal <garvit.singhal@thoughtswinsystems.com>
2025-09-26 16:00:06 +0530

    added CORS origin

--- server/src/main.py
+++ server/src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import asyncio
 from src.db.session import Base,engine
 from src.api import users
@@ -12,6 +13,18 @@ if __name__ == "__main__":
     asyncio.run(init_models())
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router,prefix="/users",tags=["Users"])
 
